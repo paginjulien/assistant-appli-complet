15dcaddeb27bb30903fcc5584efe6a4df902a3a4
paginjulien <julien.pagin.pv@gmail.com>
2025-05-24 06:57:45 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,10 @@
-// Fichier main.jsx de l'application assistant
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './App.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
